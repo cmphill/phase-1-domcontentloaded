9c0f108529b1072ce6f97ceda22ba06899c59f44
Cooper <cmphill@pm.me>
2023-06-15 16:56:39 -0600

    added a querySelector to update the content of <p>

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+console.log('the dom has loaded.')
+const mainText = document.querySelector("p#text")
+mainText.textContent = 'This is really cool!'
+mainText.append
+})
+
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
